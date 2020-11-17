9b7d6c8def4b016b954300c359064533066b904d
Rafli <raflisatriandi@outlook.co.id>
2020-11-17 21:15:28 +0700

    learning things

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ let app = express();
 let http = require("http").createServer(app);
 let io = require("socket.io")(http);
 
+let room = ["123", "456", "789"];
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/pages.html");
 });
 
 io.on("connection", (socket) => {
     console.log("user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+    
+    room.forEach(num => {
+        socket.on(num, (msg) => {
+            console.log("message on room "+num+":"+msg);
+        });
+    });
 });
 
-// app.listen(3000, ()=>{
-//     console.log("Hello, world!");
-// });
 
 http.listen(3000, ()=>{
     console.log("start");
